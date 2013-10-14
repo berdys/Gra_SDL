3fe7764c818fd74ea0bd50de1fb68999de6c471e
mkolmer <diablo_vt@vp.pl>
2013-10-14 22:54:51 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,26 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 
+static void list_audio_devices(const ALCchar *devices)
+{
+        const ALCchar *device = devices, *next = devices + 1;
+        size_t len = 0;
+
+        fprintf(stdout, "Devices list:\n");
+        fprintf(stdout, "----------\n");
+        while (device && *device != '\0' && next && *next != '\0') {
+                fprintf(stdout, "%s\n", device);
+                len = strlen(device);
+                device += (len + 1);
+                next += (len + 2);
+        }
+        fprintf(stdout, "----------\n");
+}
+
+list_audio_devices(alcGetString(NULL, ALC_DEVICE_SPECIFIER));
+
+
+
 
 int main(){
   ALboolean enumeration;
