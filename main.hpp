91bc9ce06c4b1099795e200e5f4e5c867532c8aa
berdys <berdys.marcin@gmail.com>
2013-10-18 17:10:32 +0200

    Plik main.hpp do Gra_SDL

---
+++ main.hpp
@@ -0,0 +1,26 @@
+#ifndef MAIN_HPP_INCLUDED
+#define MAIN_HPP_INCLUDED
+// headers from main.cpp
+#ifdef __cplusplus
+    #include <cstdlib>
+#else
+    #include <stdlib.h>
+#endif
+
+
+#include <iostream>
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <Box2D/Box2D.h>
+#include <stdio.h>
+
+b2World *world;
+b2Body *body;
+b2Vec2 points[4];
+Uint32 start;
+
+
+
+
+#endif // MAIN_HPP_INCLUDED
