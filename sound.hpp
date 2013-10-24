4407433cad364a7d18e28123a9ad8af1db494486
mkolmer <diablo_vt@vp.pl>
2013-10-24 14:21:42 +0200

    Some changes

--- sound.hpp
+++ sound.hpp
@@ -14,11 +14,11 @@ static inline ALenum to_al_format(short channels, short samples);
 
 static ALCdevice *device;
 static ALfloat listenerOri[] = { 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f };
-static ALuint source;
+static ALuint source[5];
 static ALuint buffer[16];
 static WaveInfo *wave;
 static void *bufferData,*bufferData1,*bufferData2,*bufferData3,*bufferData4,*bufferData5,*bufferData6,*bufferData7,*bufferData8,*bufferData9;
-static int ret;
+static unsigned int ret;
 static ALint source_state;
 static ALCcontext *context;
 void init();
@@ -86,13 +86,24 @@ void init()
     alListenerfv(AL_ORIENTATION, listenerOri);
 
     check_errors("Dodanie Słuchacza");
-    alGenSources((ALuint)1, &source);
+
+    alGenSources((ALuint)5, source);
     check_errors("Wygenerowanie źródła dzwięku");
-    alSourcef(source, AL_PITCH, 1);
-    alSourcef(source, AL_GAIN, 1);
-    alSource3f(source, AL_POSITION, 0, 0, 0);
-    alSource3f(source, AL_VELOCITY, 0, 0, 0);
-    alSourcei(source, AL_LOOPING, AL_FALSE);
+    alSourcef(source[0], AL_PITCH, 1);
+    alSourcef(source[0], AL_GAIN, 1);
+    alSource3f(source[0], AL_POSITION, 0, 0, 0);
+    alSource3f(source[0], AL_VELOCITY, 0, 0, 0);
+    alSourcei(source[0], AL_LOOPING, AL_FALSE);
+
+        for(int i=1;i<5;i++){
+            alSourcef(source[i], AL_PITCH, 1);
+            alSourcef(source[i], AL_GAIN, 1);
+            alSource3f(source[i], AL_POSITION, 0, 0, 0);
+            alSource3f(source[i], AL_VELOCITY, 0, 0, 0);
+            alSourcei(source[i], AL_LOOPING, AL_FALSE);
+        }
+
+
     check_errors("Dodanie źródeł");
 
 
@@ -171,21 +182,36 @@ void init()
 
 void play(int a)
 {
-    //nowe źródło
-    alSourcei(source, AL_BUFFER, buffer[a]);
-    alSourcePlay(source);
+
+
+    bool search=true;
+    int nr=0;
+    while(search){
+        alGetSourcei(source[nr], AL_SOURCE_STATE, &source_state);
+        if(source_state==AL_PLAYING){
+                nr++;
+        }else{
+            search=false;
+            alSourcei(source[nr], AL_BUFFER, buffer[a]);
+            alSourcePlay(source[nr]);
+
+        }
+    }
+
+    //alSourcei(source[0], AL_BUFFER, buffer[a]);
+    //alSourcePlay(source[0]);
 }
 
 void clean()
 {
-    while (source_state == AL_PLAYING){
+   /* while (source_state == AL_PLAYING){
 
         alGetSourcei(source, AL_SOURCE_STATE, &source_state);
         check_errors("Pętla czekajaca na koniec odtworzenia");
 
-    }
-    alDeleteSources(1, &source);
-    alDeleteBuffers(1, &buffer[0]);
+    }*/
+    alDeleteSources(5, source);
+    alDeleteBuffers(16, buffer);
     device = alcGetContextsDevice(context);
     alcMakeContextCurrent(NULL);
     alcDestroyContext(context);
